d67f17ea70ed170e462cbe529719abacc62a2cf5
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-04 14:32:27 -0500

    New Uri Judge challenge added

---
+++ PlayingWithOperators.py
@@ -0,0 +1,16 @@
+
+import fileinput
+
+class PlayingWithOperators():
+
+    def challenge_input(self):
+        pass
+
+    def challenge_output(self, user_input):
+        pass
+
+if __name__ == '__main__':
+
+    pwo = PlayingWithOperators()
+    user_input = pwo.challenge_input()
+    pwo.challenge_output(user_input)
