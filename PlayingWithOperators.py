aa6f597930a75ebb608a2e60cec0ba206e104bfa
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-04 23:02:41 -0500

    Almost done

--- PlayingWithOperators.py
+++ PlayingWithOperators.py
@@ -6,43 +6,62 @@ class PlayingWithOperators():
     def challenge_input(self):
         user_input = list()
         for line in fileinput.input():
-            user_input.append(line)
+            user_input.append([int(i) for i in line.split()])
         return user_input
 
     def challenge_output(self, user_input):
-        cases = int(user_input[0])
+        cases = user_input[0][0]
         index = 1
+        add = True
         for i in range(cases):
-            first_line = user_input[index].split()
+            first_line = user_input[index]
             index += 1
-            game_elements = [int(x) for x in user_input[index].split()] 
+            game_elements = user_input[index]
             index += 1
-            replacements = int(first_line[1])
-            print(self.__start_game(game_elements) )
+            replacements = first_line[1]
+            print(self.__start_game(game_elements, add) )
             for j in range(replacements):
-                numbers_to_switch = [int(x) for x in user_input[index].split()]
+                numbers_to_switch = user_input[index]
                 index += 1
                 a = numbers_to_switch[0]
                 b = numbers_to_switch[1]
                 game_elements[a - 1] = b
-                print(self.__start_game(game_elements) )
+                print(self.__start_game(game_elements, add) )
 
-    def __start_game(self, game_elements):
-        return game_elements
-        """length = len(game_elements)
-        if length == 1:
-            winner = 'Rusa'
-            if game_elements[0] % 2 == 0:
-                winner = 'Sanches'
-            return '{} {}'.format(game_elements[0], winner)
-        
+    def __start_game(self, game_elements, add):
+        n = self.__get_n(game_elements, add)
+        return self.__get_winner(n, True)
+
+    def __get_n(self, game_elements, add):
+        n_list = list()
+        length = len(game_elements)
+        is_odd = False
+        if length <= 1:
+            return game_elements[0]
         if length % 2 != 0:
+            is_odd = True
             length -= 1
-        
-        new_game_elements = list()
         for i in range(0, length, 2):
-            result = game_elements[i] + game_elements[i + 1]
-            new_game_elements.append()"""
+            n_list.append(self.__switch_operator(game_elements[i], game_elements[i + 1], add) )
+        if is_odd:
+            n_list.append(game_elements[-1])
+        return self.__get_n(n_list, not add)
+
+    def __switch_operator(self, n, m, add):
+        if add:
+            return n + m
+        return n - m
+
+    def __get_winner(self, number, flag):
+        if flag == 1:
+            winner = 'Rusa'
+            if number % 2 == 0:
+                winner = 'Sanches'
+            return '{} {}'.format(number, winner)
+        winner = 'Sanches'
+        if number % 2 == 0:
+            winner = 'Rusa'
+        return '{} {}'.format(number, winner)
 
 if __name__ == '__main__':
     pwo = PlayingWithOperators()
