fc579d4d4ebcb8977181de96aa69ddd65cc19377
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-08 13:18:03 -0500

    Commented graph class added

--- Graph.py
+++ Graph.py
@@ -1,34 +1,75 @@
 class Graph():
-
+    """
+    Graph class.
+    """
     class Node():
-
+        """
+        Internal Node class.
+        """
         def __init__(self, value):
+            """
+            Constructor.
+
+            value   The value of this node (string).
+            """
+            # Sring value of this node.
             self.value = value
+            # Mark of this node (Use this to walk the graph).
             self.mark = False
+            # dictionary of connections to this node.
             self.connections = dict()
 
     def __init__(self, *args):
+        """
+        Construtor.
+
+        value   Nodes of this graph(string).
+        """
         self.nodes_dict = dict()
         for n in args:
             node = self.Node(n)
             self.add_node(node)
 
     def new_node(self, value, *args):
+        """
+        Creates a new node.
+
+        value   The value of this node (string).
+        args    The connections with this node (strings).
+        """
         node = self.Node(value)
         self.add_connections(node, args)
         return node
 
     def add_node(self, node, *args):
+        """
+        Adds a new node to the graph.
+
+        node    Node to be added to the graph.
+        args    Connections with this node.
+        """
         if node.value not in self.nodes_dict:
             self.nodes_dict[node.value] = node
         self.add_connections(node, args)
 
     def find_node(self, value):
+        """
+        Finds a node with a given value.
+        Returns the node in the graph or None otherwise.
+
+        value   The value of the node to find (string).
+        """
         if value in self.nodes_dict:
             return self.nodes_dict[value]
         return None
 
     def add_connections(self, node, *args):
+        """
+        Adds new connections to the given node.
+        
+        node    The node to add connections.
+        args    The connections with the node (string).
+        """
         for e in args:
             n = self.find_node(e)
             if n is None:
@@ -37,6 +78,10 @@ class Graph():
                 node.connections[e] = n
 
     def has_loops(self):
+        """
+        Return True if the graph has loops and
+        False otherwise.
+        """
         for k, v in self.nodes_dict.items():
             v.mark = True
             for k1, v1 in v.connections.items():
@@ -47,6 +92,9 @@ class Graph():
         return False
 
     def __erase_marks(self):
+        """
+        Erase all marks of the nodes dict.
+        """
         for k, v in self.nodes_dict.items():
             v.mark = False
 
