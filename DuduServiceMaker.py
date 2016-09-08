f5efcb90ce5e33526d7291e0fa5b2b81aba55160
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-07 22:53:36 -0500

    Graph challenge

---
+++ DuduServiceMaker.py
@@ -0,0 +1,17 @@
+import fileinput
+
+class DuduServiceMaker():
+    """
+    Challenge number 1610 from
+    https://www.urionlinejudge.com.br/judge/es/
+    """
+    def challenge_in(self):
+        pass
+
+    def challenge_out(self, user_in):
+        pass
+
+if __name__ == '__main__':
+    dsm = DuduServiceMaker()
+    user_in = dsm.challenge_in()
+    dsm.challenge_out(user_in)
