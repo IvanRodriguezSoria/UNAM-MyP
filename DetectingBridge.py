e15424bf99d1183bc84b2670c36ca887f7197409
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-12 22:55:28 -0500

    Uri is a pice of shit. Detecting bridge not working, needs tarjan algorith.

--- DetectingBridge.py
+++ DetectingBridge.py
@@ -28,25 +28,7 @@ class DetectingBridges():
             x = int(x_and_y[0])
             y = int(x_and_y[1])
             graph[x] = y
-        #######################################
-        print(self.__aux(graph, 1, dict(), list() ) )
-
-    def __aux(self, graph, index, aux_dict, e_list):
-        index = graph[index]
-        if index == None:
-            return len(e_list)
-        if index in aux_dict:
-            return self.__aux_backwards(index, e_list)
-        aux_dict[index] = 0
-        e_list.append(index)
-        return self.__aux(graph, index, aux_dict, e_list)
-
-    def __aux_backwards(self, index, e_list):
-        counter += 1
-        for i in e_list:
-            counter += 1
-            if i == index:
-                return counter
+        # TODO Needs Tarjan algorith.
 
 if __name__ == '__main__':
     db = DetectingBridges()
