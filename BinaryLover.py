bc52f738ddb8cfe58d9cd1d704e6aab0b08b61ec
IvanRodriguezSoria <vendeta.ivan@hotmail.com>
2016-09-20 12:16:07 -0500

    BinaryLover not working

--- BinaryLover.py
+++ BinaryLover.py
@@ -1,30 +1,48 @@
 import fileinput
 
 class BinaryLover():
-
-    #TODO This takes too much time. Need to workout the solution.
+    """
+    Programming challenge number #TODO.
+    From: 
+    """
     def challenge_in(self):
+        """
+        Input for this challenge.
+        For every case prints the result.
+        """
+        b_list = self.integer_to_binary()
         for line in fileinput.input():
             n = int(line)
-            for i in range(2, 1000000000000):    
-                m_str = str(n * i)
-                if self.check_01(m_str):
-                    print(m_str)
-                    break
-                if len(m_str) > 1000000000000:
-                    print(-1)
-                    break
+            print(self.get_exact_division(n, b_list) )
 
-    def check_01(self, n):
-        for char in n:
-            if char == '1' or char == '0':
-                continue
-            else:
-                return False
-        return True
+    def get_exact_division(self, n, b_list):
+        """
+        For every decimal number in "b_list", returns the first
+        number that can be divided exactly by n, if this number
+        exist. If this number don't exist returns or is bigger
+        than 10¹², returns -1.
 
-    def challenge_out(self, user_in):
-        pass
+        n       The number that divides the elements of "b_list".
+        b_list  A list of all the permutations of 2 numbers (1 and 0).
+                From 1 to 10¹²
+        """
+        for i in b_list:
+            if i % n == 0:
+                return i
+        return -1
+
+    def integer_to_binary(self):
+        """
+        Converts the decimal numbers from 1 to 496 to his binary
+        representation. Then converts every binary number to a decimal
+        one and returs a list containing this numbers.
+        """
+        b_list = list()
+        for i in range(1, 4097):
+            n_bin = bin(i)[2:]
+            n_int = int(n_bin)
+            b_list.append(n_int)
+        return b_list
 
 if __name__ == '__main__':
     bl = BinaryLover()
